0bcb92db67701c40f91c48f9408365648be16ded
Bala Kumar S <balakumar_s@pm.me>
2024-06-13 22:00:06 +0530

    regex fix

--- frontend/src/util/appUtil.js
+++ frontend/src/util/appUtil.js
@@ -9,21 +9,21 @@ module.exports = {
         document.getElementById(`${fieldName}Component`).style.marginBottom = "2vh";
     },
     validateEmail: function (fieldValue, fieldName, submitAction) {
-        let validField = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(fieldValue);
-        if (!validField && (fieldValue || submitAction)) {
+        let isValidField = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(fieldValue);
+        if (!isValidField && (fieldValue || submitAction)) {
             module.exports.showInputFieldError(fieldName, `Invalid ${fieldName}`);
         } else {
             module.exports.hideInputFieldError(fieldName);
         }
-        return validField;
+        return isValidField;
     },
     validatePassword: function (fieldValue, fieldName, submitAction) {
-        let validField = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(fieldValue);
-        if (!validField && (fieldValue || submitAction)) {
+        let isValidField = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`.~+!@#$%^&*)(_={}[\]|\\:";'<>,/-?])[A-Za-z\d`.~+!@#$%^&*)(_={}[\]|\\:";'<>,/-?]{8,}$/.test(fieldValue);
+        if (!isValidField && (fieldValue || submitAction)) {
             module.exports.showInputFieldError(fieldName, `Invalid ${fieldName}`);
         } else {
             module.exports.hideInputFieldError(fieldName);
         }
-        return validField;
+        return isValidField;
     }
 }
